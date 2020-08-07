ffb1690c93209ca1e918befba836d4c1634e7be8
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-08-07 23:18:01 +0530

    Add validation to login form

--- src/components/common/input.js
+++ src/components/common/input.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-export default function Input({name, label, value, onChange}) {
-    return (
-        <div className="form-group">
-            <label htmlFor={name}>Username</label>
-            <input
-                value={value}
-                onChange={onChange}
-                className="form-control"
-                name={name}
-                id={name}
-                type="text" />
-        </div>
-    )
+export default function Input({ name, label, value, onChange, error }) {
+	return (
+		<div className="form-group">
+			<label htmlFor={name}>{label}</label>
+			<input
+				value={value}
+				onChange={onChange}
+				className="form-control"
+				name={name}
+				id={name}
+				type="text"
+
+			/>
+
+			{error && <div className="alert alert-danger">{error}</div>}
+		</div>
+	)
 }
--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,39 +1,80 @@
 import React, { Component } from 'react'
 import Input from './common/input';
+import Joi from 'joi-browser';
+
 export default class LoginForm extends Component {
     state = {
-        account: { username: '', password: '' }
+        account: { username: '', password: '' },
+        errors: {}
     }
+
+    schema = Joi.object({
+        username: Joi.string().required().label("Username"),
+        password: Joi.string().required().label("Password")
+    });
+
+    validate = () => {
+        const result = this.schema.validate(this.state.account, { abortEarly: false });
+        console.log(result);
+        if (!result.error) return null;
+
+        const errors = {};
+        for (let item of result.error.details)
+            errors[item.path[0]] = item.message;
+        return errors;
+    }
+
     handleSubmit = e => {
         e.preventDefault();
 
+        const errors = this.validate();
+        this.setState({ errors: errors || {} });
+        if (errors) return;
     }
-    handleChange = ({currentTarget:input}) => {
+
+    validateProperty = ({ name, value }) => {
+        const obj = { [name]: value };
+        const schema = Joi.object({ [name]: Joi.string().required() });
+        const { error } = schema.validate(obj);
+        return error ? error.details[0].message : null;
+    }
+
+    handleChange = ({ currentTarget: input }) => {
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = { ...this.state.account };
         account[input.name] = input.value;
-        this.setState({ account });
+
+        this.setState({ account, errors });
     }
 
     render() {
-        const { account } = this.state;
+        const { account, errors } = this.state;
         return (
             <div>
                 <h1>Login Form</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <Input 
-                        name="username" 
+                    <Input
+                        name="username"
                         label="Username"
                         value={account.username}
                         onChange={this.handleChange}
+                        error={errors.username}
                     />
-                    <Input 
-                        name="password" 
+                    <Input
+                        name="password"
                         label="Password"
                         value={account.password}
                         onChange={this.handleChange}
+                        error={errors.password}
                     />
-                    
-                    <button className="btn btn-primary">Login</button>
+
+                    <button className="btn btn-primary" disabled={this.validate()}>
+                        Login
+                    </button>
                 </form>
             </div>
         )
